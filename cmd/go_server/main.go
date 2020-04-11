5a3bb3afe6a03f0ef3a0d14fa1d2ec4901baeeca
Spyridon Ziangos <sziangos@gmail.com>
2020-04-11 19:51:01 -0400

    wfl: add db, get server up and connected to db, get front end connected with sample react code

--- cmd/go_server/main.go
+++ cmd/go_server/main.go
@@ -1,17 +1,106 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
-	"pkg/go_server"
+	server "pkg/go_server"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	bolt "go.etcd.io/bbolt"
 )
 
+var (
+	allowedOrigins = handlers.AllowedOrigins([]string{"*"})
+	allowedHeaders = handlers.AllowedHeaders([]string{"Accept", "DNT", "Content-Type", "Referer", "User Agent", "Sec-Fetch-Dest"})
+	allowedCredentials = handlers.AllowCredentials()
+	allowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
+)
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", server.HomeHandler)
-	r.HandleFunc("/groups", server.GroupsHandler)
-	log.Println("serving localhost:8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	router := mux.NewRouter()
+	db, err := bolt.Open("/Users/Spyro/Developer/go/src/db/wfl.db", 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rh := &server.RouteHandler{Db: server.Database{Db: db}}
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	router.HandleFunc("/v1/group/query/{groupId}", func(w http.ResponseWriter, r *http.Request) {
+		b, err := rh.GroupQueryHandler(w, r)
+		if err != nil {
+			log.Printf("GroupQueryHandler error %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(b)
+	})
+
+	router.HandleFunc("/v1/group/create", func(w http.ResponseWriter, r *http.Request) {
+		err := rh.GroupCreateHandler(w, r)
+		if err != nil {
+			log.Printf("GroupCreateHandler erro %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	spa := spaHandler{staticPath: "/Users/Spyro/Developer/go/src/web/go_web/public", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      handlers.CORS(allowedOrigins, allowedHeaders, allowedMethods, allowedCredentials)(router),
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Print("Serving on 127.0.0.1:8080")
+	log.Fatal(srv.ListenAndServe())
 }
--- pkg/go_server/server.go
+++ pkg/go_server/server.go
@@ -1,14 +1,118 @@
 package server
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	bolt "go.etcd.io/bbolt"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type RouteHandler struct {
+	Db Database
 }
 
-func GroupsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello groups")
+// Database is a struct containing a bolt db
+type Database struct {
+	Db *bolt.DB
+}
+
+// User is a struct that represents a user
+type User struct {
+	Username          string
+	Name              string
+	Zipcode           string
+	Groups            []Group
+	RestaurantsTried  []string
+	RestaurantsMissed []string
+}
+
+// Group is a struct that represents a group
+type Group struct {
+	ID         int   `json:"groupID"`
+	GroupName         string   `json:"groupName"`
+	Gembers           []User   `json:"groupMembers"`
+	RestaurantsTried  []string `json:"restaurantsTried,omitempty"`
+	RestarantsMissed  []string `json:"restaurantsMissed,omitempty"`
+	CollectiveZipcode string   `json:"collectiveZip,omitempty"`
+}
+
+// Restaurant is a struct that represents a restaurant
+type Restaurant struct {
+	YelpID        string
+	Liked         bool
+	MichelinStars float64
+}
+
+// NewStore opens a database, and retruns a Database struct
+func NewRouter() (*RouteHandler, error) {
+	db, err := bolt.Open("/Users/Spyro/Developer/go/src/db/wfl.db", 0666, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	database := &Database{Db: db}
+	return &RouteHandler{Db: *database}, nil
+}
+
+func (db *Database) CreateGroup(g *Group) error {
+	return db.Db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("groups"))
+		if err != nil {
+			return fmt.Errorf("Write: CreateBucket: %v", err)
+		}
+
+		id, _ := b.NextSequence()
+		g.ID = int(id)
+
+		buf, err := json.Marshal(g)
+		if err != nil {
+			return err
+		}
+		
+		return b.Put(itob(g.ID), buf)
+	})
+}
+
+func (db *Database) QueryGroup(id int) ([]byte) {
+	var group []byte
+	db.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("groups"))
+		group = b.Get(itob(id))
+		return nil
+	})
+	return group
+}
+
+func (h *RouteHandler) GroupQueryHandler(w http.ResponseWriter, r *http.Request) ([]byte, error) {
+	groupID := mux.Vars(r)["groupId"]
+	id, err := strconv.Atoi(groupID)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return nil, err
+	}
+	b := h.Db.QueryGroup(id)
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return nil, nil
+	}
+	return b, err
+}
+
+func (h *RouteHandler) GroupCreateHandler(w http.ResponseWriter, r *http.Request) (error) {
+	var g Group
+	err := json.NewDecoder(r.Body).Decode(&g)
+	if err != nil {
+		return err
+	}
+	return h.Db.CreateGroup(&g)
+}
+
+func itob(v int) []byte {
+    b := make([]byte, 8)
+    binary.BigEndian.PutUint64(b, uint64(v))
+    return b
 }
